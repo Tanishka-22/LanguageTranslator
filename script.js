10ff339f6d11c2d697fb11569ce447357067e643
Tanishka-22 <tanishkashivhare22@gmail.com>
2024-08-24 23:41:19 +0530

    ew

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const fromText = document.querySelector(".from-text"),
 toText = document.querySelector(".to-text"),
-exchangeIcon = document.querySelector(".exchange"),
+exchageIcon = document.querySelector(".exchange"),
 selectTag = document.querySelectorAll("select"),
-icons = document.querySelectorAll(".row i")
-translateBtn = document.querySelector("button"); 
+icons = document.querySelectorAll(".row i");
+translateBtn = document.querySelector("button"),
 
-
-selectTag.forEach ((tag,id) => {
+selectTag.forEach((tag, id) => {
     for (let country_code in countries) {
         let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
         let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
@@ -14,7 +13,7 @@ selectTag.forEach ((tag,id) => {
     }
 });
 
-exchangeIcon.addEventListener("click", () => {
+exchageIcon.addEventListener("click", () => {
     let tempText = fromText.value,
     tempLang = selectTag[0].value;
     fromText.value = toText.value;
@@ -23,11 +22,11 @@ exchangeIcon.addEventListener("click", () => {
     selectTag[1].value = tempLang;
 });
 
-fromText.addEventListener("click", () => {
+fromText.addEventListener("keyup", () => {
     if(!fromText.value) {
-        toText.value = "" ;
+        toText.value = "";
     }
-}); 
+});
 
 translateBtn.addEventListener("click", () => {
     let text = fromText.value.trim(),
